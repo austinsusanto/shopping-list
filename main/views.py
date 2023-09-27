0c6c12d434d5dc0df6a23d59da7ec8a240b3d254
Austin <austinsusanto@gmail.com>
2023-09-27 16:00:56 +0700

    Update: Tutorial 4

--- main/views.py
+++ main/views.py
@@ -79,6 +79,31 @@ def logout_user(request):
     return response
 
 
+def edit_product(request, id):
+    # Get product berdasarkan ID
+    product = Product.objects.get(pk=id)
+
+    # Set product sebagai instance dari form
+    form = ProductForm(request.POST or None, instance=product)
+
+    if form.is_valid() and request.method == "POST":
+        # Simpan form dan kembali ke halaman awal
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_product.html", context)
+
+
+def delete_product(request, id):
+    # Get data berdasarkan ID
+    product = Product.objects.get(pk=id)
+    # Hapus data
+    product.delete()
+    # Kembali ke halaman awal
+    return HttpResponseRedirect(reverse('main:show_main'))
+
+
 def show_xml(request):
     data = Product.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
