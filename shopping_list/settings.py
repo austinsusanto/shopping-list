84d3190cd87c152619a1cf6db523f82c3c0c8104
Austin <austinsusanto@gmail.com>
2024-05-18 00:10:18 +0700

    feat: Set up deployment configuration for production

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -138,3 +138,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://web-production-d16e.up.railway.app']
